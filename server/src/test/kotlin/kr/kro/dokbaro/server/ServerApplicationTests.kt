6c5435b8bef761269faf689dab718adda5218ec1
Hyeonjun Park <phjppo0918@gmail.com>
2024-07-07 19:46:17 +0900

    init

---
+++ server/src/test/kotlin/kr/kro/dokbaro/server/ServerApplicationTests.kt
@@ -0,0 +1,11 @@
+package kr.kro.dokbaro.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
