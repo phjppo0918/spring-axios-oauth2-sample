10d50062edb8f904c33177907a9fa8d3143482a3
Hyeonjun Park <phjppo0918@gmail.com>
2024-07-09 23:17:19 +0900

    reformat

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,21 +1,10 @@
 <script>
-    import kakaoSignUpButton from '$lib/images/kakao_signup.png';
-    import googleSignUpButton from '$lib/images/google_signup.png';
     import kakaoLoginButton from '$lib/images/kakao_login.png';
-    import googleLoginButton from '$lib/images/google_login.png';
 
-
-
-    // 회원가입 -> 회원가입 시 로그인 자동으로 됨
     function kakaologin() {
         window.location.href = 'http://localhost:8080/api/auth/redirect/kakao'
     }
-
-    // 로그인 -> 회원가입 안된 계정이면 error
-
-
-    // 회원 탈퇴
 </script>
 
 <h1>로그인</h1>
-<img src={kakaoLoginButton} on:click={kakaologin}/>
+<img on:click={kakaologin} src={kakaoLoginButton}/>
--- client/src/routes/oauth2/redirected/kakao/+page.svelte
+++ client/src/routes/oauth2/redirected/kakao/+page.svelte
@@ -1,24 +1,19 @@
 <script>
-    import { onMount } from 'svelte';
-    import { page } from '$app/stores'
+    import {onMount} from 'svelte';
+    import {page} from '$app/stores'
 
     import axios from "axios";
 
     axios.defaults.withCredentials = true;
     axios.defaults.baseURL = "http://localhost:8080/api";
 
-
-
     onMount(() => {
-
         const code = $page.url.searchParams.get('code')
 
-        axios.post("/auth/login/kakao?code="+code).then(res => {
-            alert(res.data.accessToken+ " " + res.data.refreshToken)
+        axios.post("/auth/login/kakao?code=" + code).then(res => {
+            alert(res.data.accessToken + " " + res.data.refreshToken)
         })
     });
-
-   // $: unsubscribe();
 </script>
 
 <div>
