a91e12140a158c47363d82f928b0b3a17760b7d2
Hyeonjun Park <phjppo0918@gmail.com>
2024-07-09 21:40:55 +0900

    클라이언트 생성

--- client/src/routes/oauth2/redirected/kakao/+page.svelte
+++ client/src/routes/oauth2/redirected/kakao/+page.svelte
@@ -1,36 +1,22 @@
 <script>
     import { onMount } from 'svelte';
+    import { page } from '$app/stores'
 
     import axios from "axios";
 
     axios.defaults.withCredentials = true;
     axios.defaults.baseURL = "http://localhost:8080/api";
 
-    let location;
-    // const unsubscribe = page.subscribe(value => {
-    //     location = value;
-    // });
 
-    const handleOAuthKakao = async (code) => {
-        try {
-            // 카카오로부터 받아온 code를 서버에 전달하여 카카오로 회원가입 & 로그인한다
-            //const response = await get(`http://localhost:8080/api/auth/login/kakao?code=${code}`);
-            //const data = response.data; // 응답 데이터
-            //alert("로그인 성공: " + data);
-            //goto("/success");
-        } catch (error) {
-           // goto("/fail");
-            print(error)
-        }
-    };
 
     onMount(() => {
-        const searchParams = new URLSearchParams(location.url.search);
-        const code = searchParams.get('code');  // 카카오는 Redirect 시키면서 code를 쿼리 스트링으로 준다.
-        if (code) {
-            alert("CODE = " + code);
-            //handleOAuthKakao(code);
-        }
+
+        const code = $page.url.searchParams.get('code')
+
+        axios.post("/auth/login/kakao?code="+code).then(res => {
+            alert(res)
+            console.log(res)
+        })
     });
 
    // $: unsubscribe();
